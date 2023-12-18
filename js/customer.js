4b83625265be8709ebc65415f52fdf2843d790d7
ngqvan <ngqvan@VNSGNWN1027014>
2023-12-18 16:45:34 +0700

    loaddata

---
+++ js/customer.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+function loadData() {
+    $.each(data, function (idex, item) {
+        var trHTML = $(`<tr>
+                         <td>`+ item.employeeCode + `</td>
+                         <td>`+ item.employeeNam + `</td>
+                         <td>`+ item.Email + `</td>
+                         <td>`+ item.Address + `</td>
+                         <td>`+ item.Mobile +`</td>
+                            </tr>`);
+        $('.grid tbody').append(trHTML);
+    })
+}
+
+var data = [
+    {
+        employeeCode : "NV001",
+        employeeNam : "Quang Văn",
+        Email : "asdasd@gmail.com",
+        Address : "Ben Tre",
+        Mobile : "035242341"
+    },
+    {
+        employeeCode : "NV:002",
+        employeeNam : "Quang Văn 1",
+        Email : "gadhiue@gmail.com",
+        Address : "Đà Nẵng",
+        Mobile : "05234234"
+    },
+    {
+        employeeCode : "NV003",
+        employeeNam : "Quang Văn 2",
+        Email : "kokoaasd@gmail.com",
+        Address : "HCM",
+        Mobile : "08832424"
+    }
+]
